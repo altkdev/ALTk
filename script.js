c0a44ff5bb4b0dd94b7768a56da967c35de665f4
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2022-03-17 17:40:51 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-      document.addEventListener('keydown', (e) => {
+$.backstretch("hhhhh.jpg")
+document.addEventListener('keydown', function(e) {
     if (e.key.toLowerCase() === 'k' && e.altKey) {
         e.preventDefault();
 
          // please dont 
          alert('Look');
          window.open("https://youtu.be/dPmZqsQNzGA?t=19");
-         }
-     if (e.key.toLowerCase() === 'a' && e.ctrlKey) {
-        e.preventDefault();
+    }
+    
+    if(e.ctrlKey){
+        if (e.key.toLowerCase() === 'a') {
+            e.preventDefault();
 
-         // please dont 
-         alert('Look at me');
-         window.open("https://krishrajchal.github.io/THE-MOST-EPIC-WEBSITE/");
-         }
-    if (e.key.toLowerCase() === 'b' && e.ctrlKey) {
-        e.preventDefault();
+            // please dont 
+            alert('Look at me');
+            window.open("https://krishrajchal.github.io/THE-MOST-EPIC-WEBSITE/");
+            }
+        if (e.key.toLowerCase() === 'b') {
+            e.preventDefault();
 
-         // please dont 
-         alert('Look at me');
-         window.open("https://www.youtube.com/watch?v=lDK9QqIzhwk/");
-         }
-    if (e.key.toLowerCase() === 'c' && e.ctrlKey) {
-        e.preventDefault();
+            // please dont 
+            alert('Look at me');
+            window.open("https://www.youtube.com/watch?v=lDK9QqIzhwk/");
+            }
+        if (e.key.toLowerCase() === 'c') {
+            e.preventDefault();
 
-         // please dont 
-         alert('Look at me');
-         window.open("https://www.youtube.com/watch?v=bvWRMAU6V-c/");
-         } 
-    if (e.key.toLowerCase() === 'd' && e.ctrlKey) {
-        e.preventDefault();
+            // please dont 
+            alert('Look at me');
+            window.open("https://www.youtube.com/watch?v=bvWRMAU6V-c/");
+            } 
+        if (e.key.toLowerCase() === 'd') {
+            e.preventDefault();
 
-         // please dont 
-         alert('Look at me');
-         window.open("https://www.youtube.com/watch?v=2UcZWXvgMZE/");
-         }
-    if (e.key.toLowerCase() === 'e' && e.ctrlKey) {
-        e.preventDefault();
+            // please dont 
+            alert('Look at me');
+            window.open("https://www.youtube.com/watch?v=2UcZWXvgMZE/");
+            }
+        if (e.key.toLowerCase() === 'e') {
+            e.preventDefault();
 
-         // please dont 
-         alert('Look at me');
-         window.open("https://www.youtube.com/watch?v=E3YmLs7XqIo");
-         }
-    if (e.key.toLowerCase() === 'f' && e.ctrlKey) {
-        e.preventDefault();
+            // please dont 
+            alert('Look at me');
+            window.open("https://www.youtube.com/watch?v=E3YmLs7XqIo");
+            }
+        if (e.key.toLowerCase() === 'f') {
+            e.preventDefault();
 
-         // please dont 
-         alert('Look at me');
-         window.open("https://www.youtube.com/watch?v=hHB1Ikzfpmc");
-         }
+            // please dont 
+            alert('Look at me');
+            window.open("https://www.youtube.com/watch?v=hHB1Ikzfpmc");
+            }
+    }
 });
