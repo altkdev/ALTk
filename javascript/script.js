4dc8dd9738d22585cca2e704f6b3b4e72b50d736
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2022-04-08 20:44:58 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -13,6 +13,7 @@ setInterval(() => {
 }, 30000);
 
 document.onkeydown = function (e){
+    e.preventDefault();
     if(secrets){
         keylog += e.key;
         if(keylog.includes("awesome")){
@@ -79,42 +80,31 @@ function a(e) {
     }
     if (e.ctrlKey && secrets) {
         if (e.key.toLowerCase() === 'a') {
-            e.preventDefault();
-
             // please dont 
             alert('Look at me');
             window.open("https://www.youtube.com/watch?v=GcYF1xnmZTI");
         }
         if (e.key.toLowerCase() === 'b') {
-            e.preventDefault();
-
             // please dont 
             alert('Look at me');
             window.open("https://www.youtube.com/watch?v=lDK9QqIzhwk/");
         }
         if (e.key.toLowerCase() === 'c') {
-            e.preventDefault();
-
             // please dont 
             alert('Look at me');
             window.open("https://www.youtube.com/watch?v=bvWRMAU6V-c/");
         }
         if (e.key.toLowerCase() === 'd') {
-            e.preventDefault();
             // please dont 
             alert('Look at me');
             window.open("https://www.youtube.com/watch?v=2UcZWXvgMZE/");
         }
         if (e.key.toLowerCase() === 'e') {
-            e.preventDefault();
-
             // please dont 
             alert('Look at me');
             window.open("https://www.youtube.com/watch?v=E3YmLs7XqIo");
         }
         if (e.key.toLowerCase() === 'f') {
-            e.preventDefault();
-
             // please dont 
             alert('Look at me');
             window.open("https://www.youtube.com/watch?v=hHB1Ikzfpmc");
