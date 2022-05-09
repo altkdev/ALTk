507926f125d3dc475b5a7165b196a56a1ec1afab
Krish-Rajchal <104920463+Krish-Rajchal@users.noreply.github.com>
2022-05-09 10:05:40 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,7 @@ function rand(min, max) {
   return Math.floor(Math.random() * max) + min;
 }
 
-if(/Android|webOS|iPhone|iPad|MacIntel|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if(/Android|webOS|iPhone|iPad|MacIntel|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgentData) ) {
     document.getElementById("mobile").style.visibility = "visible";
     mobile = true;
 }
