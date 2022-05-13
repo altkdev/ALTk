49c0d9705508a65afaa7a828f82c9c2b16372cd7
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-05-13 10:45:33 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -4,13 +4,13 @@ var keylog = "";
 var didcolors = false;
 var mobile = false;
 var temp = document.getElementById("vid");
-const messages = ["Do you want to delete the world?", "Do you want to delete all the beans in the world?", "Its a bird, its a plane, its another video"];
+const messages = ["Do you want to delete the world?", "Do you want to delete all the beans in the world?", "Its a bird, its a plane, its another video", "What did you just say to me boy?"];
 
 function rand(min, max) {
   return Math.floor(Math.random() * max) + min;
 }
 function randalert() {
-	alert(messages[rand(0, 3)]);
+	alert(messages[rand(1, 5)]);
 } 
  
 if(navigator.userAgent.toLowerCase().match(/mobile/i)) {
