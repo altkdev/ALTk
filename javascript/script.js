7f49dd3f8109a53afa5ee60e2477e7cd0f04c8ab
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-06-29 12:05:24 -0600

    other vid extension feature
    for mp4  use       play(whatevervideo, 0); other play(whatevervideo, "mkv") ;

--- javascript/script.js
+++ javascript/script.js
@@ -101,26 +101,26 @@ function secretChecked() {
 	if (document.getElementById('secret').checked && secrets) {
 		console.log("checked");
 		alert("YoU haVe fOUnD A scErEt");
-		play("The_Funeral")
+		play("The_Funeral", 0)
 	}
 }
 
 function secretText() {
 	if (secrets) {
 		if (document.getElementById("secretText").value == "text") {
-			play(1)
+			play(1, 0)
 		}
 		if (document.getElementById("secretText").value == "abcdefghijklmnopqrstuvwxyz") {
-			play(2)
+			play(2, 0)
 		}
 		if (document.getElementById("secretText").value == "something random") {
 			window.open("/ALTk/pages/hELLO.html")
 		}
 		if (document.getElementById("secretText").value == "69420") {
-			play(420)
+			play(420, 0)
 		}
 		if (document.getElementById("secretText").value == "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99") {
-			play(800)
+			play(800, 0)
 		}
 	}
 }
@@ -130,7 +130,7 @@ function play(file, other) {
 	if (!$("#vid").is(":visible") && !(vid.currentTime > 0 && !vid.paused && !vid.ended && vid.readyState > 2)) {
 		$("#vid").fadeToggle("slow", "linear")
 	}
-	if (other = "") {
+	if (other = "0") {
 		vid.src = "videos/" + file.toString() + ".mp4"
 	} else {
 		vid.src = "videos/" + file.toString() + other.toString()
@@ -142,38 +142,38 @@ function a_mobile(e, d) {
 	if (e == 'k' && d == "a") {
 		randalert();
 		document.getElementById("vid").style.visibility = "visible";
-		play(69)
+		play(69, 0)
 	}
 	if (d == "c" && secrets) {
 		if (e == 'a') {
 			randalert();
 			document.getElementById("vid").style.visibility = "visible";
-			play(9)
+			play(9, 0)
 		}
 		if (e == 'b') {
 			randalert();
 			document.getElementById("vid").style.visibility = "visible";
-			play(4)
+			play(4, 0)
 		}
 		if (e == 'c') {
 			randalert();
 			document.getElementById("vid").style.visibility = "visible";
-			play(10)
+			play(10, 0)
 		}
 		if (e == 'd') {
 			randalert();
 			document.getElementById("vid").style.visibility = "visible";
-			play(6)
+			play(6, 0)
 		}
 		if (e == 'e') {
 			randalert();
 			document.getElementById("vid").style.visibility = "visible";
-			play(8)
+			play(8, 0)
 		}
 		if (e == 'f') {
 			randalert();
 			document.getElementById("vid").style.visibility = "visible";
-			play(7)
+			play(7, 0)
 		}
 	}
 }
@@ -182,38 +182,38 @@ function a(e) {
 	if (e.key == 'k' && e.altKey) {
 		e.preventDefault();
 		randalert();
-		play(69)
+		play(69, 0)
 	}
 	if (e.ctrlKey && secrets) {
 		if (e.key == 'a') {
 			e.preventDefault();
 			randalert();
-			play(9)
+			play(9, 0)
 		}
 		if (e.key == 'b') {
 			e.preventDefault();
 			randalert();
-			play(4)
+			play(4, 0)
 		}
 		if (e.key == 'c') {
 			e.preventDefault();
 			randalert();
-			play(10)
+			play(10, 0)
 		}
 		if (e.key == 'd') {
 			e.preventDefault();
 			randalert();
-			play(6)
+			play(6, 0)
 		}
 		if (e.key == 'e') {
 			e.preventDefault();
 			randalert();
-			play(8)
+			play(8, 0)
 		}
 		if (e.key == 'f') {
 			e.preventDefault();
 			randalert();
-			play(7)
+			play(7, 0)
 		}
 	}
 }
@@ -221,5 +221,5 @@ function a(e) {
 function destroyPage(message) {
 	alert(message);
 	window.open("/ALTk/pages/site3.html");
-	window.close(window.location.href())
+	window.close("/ALTk/");
 }
