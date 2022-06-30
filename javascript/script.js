39a7d234877d7d0ad4197dfd81da626af73f836e
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-06-30 14:00:31 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,43 @@ var didcolors = false;
 var mobile = false;
 var temp = document.getElementById("vid");
 const messages = ["Do you want to delete the world?", "Do you want to delete all the beans in the world?", "Its a bird, its a plane, its another video", "What did you just say to me boy?"];
+let cookies = navigator.cookieEnabled;
+if (cookies = true){
+	function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
+	
+}
 function rand(min, max, other) {
 	if (other = 1) {
 		return Math.floor(Math.random() * (max - min + 1)) + min
