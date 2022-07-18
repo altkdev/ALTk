09dd8a7b55c25d1e882d41f99045bbdf81a23ad8
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-07-17 20:17:15 -0600

    Update and rename tracking.js to tracking1.js

---
+++ tracking/tracking1.js
@@ -0,0 +1,6 @@
+//clarity
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "b9gbg7mxfb");
