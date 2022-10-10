4952c218b59496f6c4d800c619b86cf501f1a626
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-10-10 09:08:29 -0600

    Update tracking1.js

--- tracking/tracking1.js
+++ tracking/tracking1.js
@@ -3,4 +3,4 @@
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "e0w806nvfp");
+    })(window, document, "clarity", "script", "b9gbg7mxfb"); //another one e0w806nvfp
