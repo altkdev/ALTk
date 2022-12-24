f2d0bbed128b99c631d1637beb6d37b5f6912a9b
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2022-12-24 14:42:16 -0700

    maby?
    Signed-off-by: Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>

--- javascript/script.js
+++ javascript/script.js
@@ -465,7 +465,7 @@ function playVideo(key) {
 //#endregion
 
 //#region Platform specific
-if (navigator.userAgent.toLowerCase().match(/mobile/i)){
+if (navigator.userAgent.toLowerCase().match(/mobile/i || /xbox/i || meta/i)){
 	document.getElementById("mobile").style.visibility = "visible";
 	console.log("You are using a mobile browser")
 }else if(navigator.platform.indexOf(("Mac") || ("IPad")) === 0){
