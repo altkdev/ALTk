266bdac7e8b09c92dd7500543126eb77fa7512ac
Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>
2023-05-22 08:48:01 -0600

    Language young man
    Signed-off-by: Owen Deepe <84058356+dr-comeemeememem@users.noreply.github.com>

--- javascript/script.js
+++ javascript/script.js
@@ -537,11 +537,11 @@ function playVideo(key) {
 var userLanguage = navigator.language || navigator.userLanguage;
 
 // Check if the current page URL contains the Google Translate pattern
-if (document.URL.indexOf('translate.google.com') === -1) {
+if (window.location.hostname.indexOf('translate.google.com') !== 0) {
   if (userLanguage.toLowerCase().substr(0, 2) !== 'en') {
     var languageParam = userLanguage.substr(0, 2);
     var redirectUrl = 'https://altk-xyz.translate.goog/?_x_tr_sch=http&_x_tr_sl=auto&_x_tr_tl=' + languageParam + '&_x_tr_hl=en&_x_tr_pto=wapp';
-    window.location.href = redirectUrl + encodeURIComponent(document.URL);
+    window.location.href = redirectUrl
   } else {
     console.log("Congratulations, you speak English!");
   }
